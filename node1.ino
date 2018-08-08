27140582970683f3fd256e9e950717d41d43f15a
notes2553 <42199924+notes2553@users.noreply.github.com>
2018-08-08 14:11:28 +0700

    Add files via upload

---
+++ node1.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial NodeSerial(D3, D2); // RX, TX
+void setup() {
+  Serial.begin(9600);
+  NodeSerial.begin(4800);
+}
+void loop() {
+  while (NodeSerial.available() > 0) {
+    float val = NodeSerial.parseFloat();
+    if (NodeSerial.read() == '\n') {
+      Serial.println(val);
+    }
+  }
+  delay(100);
+}
+
