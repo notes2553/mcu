4cd66c9560c3685a4bdb13a7942978142f29078a
notes2553 <42199924+notes2553@users.noreply.github.com>
2018-08-15 10:50:28 +0700

    Add files via upload

---
+++ node8266_conext_mobile.ino
@@ -0,0 +1,24 @@
+#include <ESP8266WiFi.h>
+const char* ssid = "note_wifi";
+const char* password = "123456789n";
+
+void setup() 
+{
+   Serial.begin(115200);
+   Serial.println("Starting...");
+
+   WiFi.begin(ssid, password);
+   while (WiFi.status() != WL_CONNECTED) 
+   {
+      delay(250);
+      Serial.print(".");
+   }
+
+   Serial.println("WiFi connected");  
+   Serial.println("IP address: ");
+   Serial.println(WiFi.localIP());
+}
+
+void loop() 
+{
+}
